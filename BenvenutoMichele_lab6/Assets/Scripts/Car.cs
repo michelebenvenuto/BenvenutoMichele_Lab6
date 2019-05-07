8666b50f59b5efdae7a8222530e705276ca124d4
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2019-05-06 18:53:54 -0600

    Final version

---
+++ BenvenutoMichele_lab6/Assets/Scripts/Car.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Car : MonoBehaviour
+{
+    public GameObject[] path;
+    private int toGo = 0;
+    private GameObject next;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toGo < path.Length)
+        {
+            if (!GetComponent<NavMeshAgent>().hasPath)
+            {
+                next = path[toGo];
+                Debug.Log(toGo);
+                toGo += 1;
+                GetComponent<NavMeshAgent>().SetDestination(next.transform.position);
+            }
+        }
+        else {
+            toGo = 0;
+        }
+    }
+}
---
+++ BenvenutoMichele_lab6/Assets/Scripts/player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(myRay, out hitInfo))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hitInfo.point);
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Car")) {
+            Destroy(gameObject);
+        }
+    }
+}
+ 
